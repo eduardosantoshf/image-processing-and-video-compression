b291c17a57da6346211032f440021d74584928a1
Eduardo Santos <eduardosantoshf@gmail.com>
2020-10-17 17:36:07 +0100

    VideoPlayer working

---
+++ 1-VideoPlayer/CMakeFiles/VideoPlayer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VideoPlayer.dir/main.cpp.o"
+  "VideoPlayer"
+  "VideoPlayer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VideoPlayer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
