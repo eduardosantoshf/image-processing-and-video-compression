024fc6a59747d05f8848b2cfb065b620af54e7b1
Eduardo Santos <eduardosantoshf@gmail.com>
2020-10-20 15:26:30 +0100

    Transform image to color spaces YUV and HSV

--- 2-Image-Processing/src/2_b.cpp
+++ 2-Image-Processing/src/2_b.cpp
@@ -10,12 +10,12 @@ int main(void)
 {
     Mat initial_image, yuv_image, hsv_image;
     initial_image = imread("../images/windowsXP.jpg");
-    //imshow("Original image", initial_image);
+    imshow("Original image", initial_image);
 
     /*------------ Convert to YUV ------------*/
 
     yuv_image = convert_to_yuv(initial_image);
-    //imshow("YUV image", yuv_image);
+    imshow("YUV image", yuv_image);
 
     vector<Mat> yuv_channels;
     split(yuv_image, yuv_channels);
@@ -25,6 +25,7 @@ int main(void)
     Mat U = -(0.148 * yuv_channels[2]) - (0.291 * yuv_channels[1]) + (0.439 * yuv_channels[0]) + 128;
     Mat V = (0.439 * yuv_channels[2]) - (0.368 * yuv_channels[1]) - (0.071 *  yuv_channels[0]) + 128;
     */
+
     imshow("YUV - Y", yuv_channels[0]);
     imshow("YUV - U", yuv_channels[1]);
     imshow("YUV - V", yuv_channels[2]);
@@ -32,7 +33,7 @@ int main(void)
     /*------------ Convert to HSV ------------*/
 
     hsv_image = convert_to_hsv(initial_image);
-    //imshow("HSV image", hsv_image);
+    imshow("HSV image", hsv_image);
 
     vector<Mat> hsv_channels;
     split(hsv_image, hsv_channels);
