39e737ef28cc184adb123f269e5c18f097551f21
Eduardo Santos <eduardosantoshf@gmail.com>
2020-11-04 14:03:08 +0000

    Added comments

--- video-player/src/BitStream.cpp
+++ video-player/src/BitStream.cpp
@@ -27,12 +27,12 @@ class BitStream {
              * 
              * @param bit value that we want to write
              */
-
-            if (bit) 
-                buff |= (1 << pos);
+            
+            if (bit)
+                buff |= (1 << pos); //add to buffer
             pos++;
 
-            if (pos == 8) {
+            if (pos == 8) { //when we have enough bits to write 1 byte (8 bits), write to the file
                 fso.open(filename, ios::binary | ios::app);
                 fso.write(reinterpret_cast<char*>(&buff), sizeof(buff) * sizeof(char));
                 fso.close();
@@ -51,12 +51,12 @@ class BitStream {
              * starting at 0
              */
 
-           int cont = 1;
+            int cont = 1;
             fsi.open(filename, ios::in | ios::binary);
             readBuff = 0;
 
             while (n > 7) {
-                cont++;
+                cont++; //check which byte we'll be reading
                 n = n - 8;
             }
 
@@ -64,10 +64,10 @@ class BitStream {
                 fsi.read(reinterpret_cast<char *>(&readBuff), 1);
             }
 
-            b = bitset<8>(readBuff);
+            b = bitset<8>(readBuff); //store the byte in a bitset
             cout << b << "\n";
 
-            cout << b.to_string()[n] << "\n";
+            cout << b.to_string()[n] << "\n"; //there has got to be a better way of doing this
             fsi.close();
 
        }
@@ -88,11 +88,22 @@ class BitStream {
        }
 
        void writeNBits(int n, int b) {
+
+           /**
+             * Function used to write N bits at a time from
+             * the given file
+             * 
+             * @param n number we want to be written in binary
+             * @param b how many bits do we want the number n
+             * to be written
+             */
+
             int a[8];
             int i;
-            int lengthA = 0;
-            int surplus = 0;
+            int lengthA = 0; //length of a
+            int surplus = 0; //numer of extra 0s we'll add at the left side of the binary number
 
+            //turn the given number into binary
             for (i = 0; n > 0; i++) {
                 a[i] = n % 2;
                 n = n / 2;
@@ -107,13 +118,13 @@ class BitStream {
             if (b >= lengthA) {
                 surplus = b - lengthA;
                 for (int j = b; j < 8; j++) {
-                    writeBit(0);
+                    writeBit(0); //write the surplus' 0s
                 }
                 for (int k = lengthA; k > 0; k--) {
-                    writeBit(a[k]);
+                    writeBit(a[k]); //write the binary number
                 }
                 for (int l = 0; l < surplus; l++) {
-                    writeBit(0);
+                    writeBit(0); //write the remaining 0s
                 }
             }
        }
@@ -127,6 +138,8 @@ class BitStream {
 
             fsiF.open("test.bin", ios::binary | ios::in);
             char c;
+
+            //read all the bytes from the binary file
             while (fsiF.get(c)) {
                 for (int i = 7; i >= 0; i--)
                     cout << ((c >> i) & 1);
