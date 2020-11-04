70a9c51671d38f7ebd11ce11289d03442e09fa54
Eduardo Santos <eduardosantoshf@gmail.com>
2020-11-04 02:48:40 +0000

    Main to test BitStream

--- video-player/src/4.cpp
+++ video-player/src/4.cpp
@@ -1,85 +1,15 @@
 #include<iostream>
 #include<fstream>
-#include "methods.cpp"
+#include "BitStream.cpp"
 
 using namespace std;
-using namespace cv;
-
-class BitStream {
-    public:
-        int pos = 0;
-        unsigned char buff = 0;
-        fstream fsi;
-        fstream fso;
-        fstream fsiF;
-        unsigned char readBuff = 0;
-        bitset<8> b;
-        string filename;
-
-        BitStream(string fn) {
-            filename = fn;
-        }
-        
-        void writeBit(int bit) {
-            if (bit) 
-                buff |= (1 << pos);
-            pos++;
-
-            if (pos == 8) {
-                fso.open(filename, ios::binary | ios::app);
-                fso.write(reinterpret_cast<char*>(&buff), sizeof(buff) * sizeof(char));
-                fso.close();
-                pos = 0;
-                buff = 0;
-            }
-        }
-
-       void readBit(int n) {
-           int cont = 1;
-            fsi.open(filename, ios::in | ios::binary);
-            readBuff = 0;
-
-            while (n > 7) {
-                cont++;
-                n = n - 8;
-            }
-
-            for (int j = 0; j < cont; j++) {
-                fsi.read(reinterpret_cast<char *>(&readBuff), 1);
-            }
-
-            b = bitset<8>(readBuff);
-            cout << b << "\n";
-
-            cout << b.to_string()[n] << "\n";
-            fsi.close();
-
-       }
-
-       void readNBits(int n) {
-            for (int i = 0; i < n; i++) {
-                readBit(i);
-            }
-       }
-
-        void readFile() {
-            fsiF.open("test.bin", ios::binary | ios::in);
-            char c;
-            while (fsiF.get(c)) {
-                for (int i = 7; i >= 0; i--)
-                    cout << ((c >> i) & 1);
-                    cout << "\n";
-            }
-            fsiF.close();
-        }
-};
 
 int main(void)
 {
 
     BitStream bs("test.bin");
     
-    /*
+    
     bs.writeBit(1);
     bs.writeBit(1);
     bs.writeBit(1);
@@ -97,9 +27,16 @@ int main(void)
     bs.writeBit(0);
     bs.writeBit(1);
     bs.writeBit(1);
-    */
     
     bs.readFile();
+
+    cout << "\n";
+
+    bs.readBit(0);
+    bs.readBit(1);
+    bs.readBit(2);
+    
+    cout << "\n";
  
     bs.readNBits(13);
 }
