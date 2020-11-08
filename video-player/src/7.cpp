bc1c8d7092c35219cac9ae3a3f1a2574c0bb623b
Eduardo Santos <eduardosantoshf@gmail.com>
2020-11-08 00:11:35 +0000

    Updated

--- video-player/src/7.cpp
+++ video-player/src/7.cpp
@@ -7,9 +7,11 @@ int main(void)
 {
     Golomb golomb("teste.txt", 4, 'w');
 
-    vector<bool> v = golomb.encode(20);
+    vector<bool> v = golomb.encode(-20);
 
     for (int i = 0; i < v.size(); i++) {
         cout << v.at(i) << "\n";
     }
+
+    cout << golomb.decode(v);
 }
