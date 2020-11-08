2df2078afc45ab23bfa85ec1221273050a3cca68
Eduardo Santos <eduardosantoshf@gmail.com>
2020-11-08 19:56:20 +0000

    Now the encoded number is stored in a binary file, using the BitStream class

--- video-player/src/7.cpp
+++ video-player/src/7.cpp
@@ -1,17 +1,60 @@
 #include "Golomb.cpp"
+#include"BitStream.cpp"
 #include<vector>
 
 using namespace std;
 
 int main(void) 
 {
-    Golomb golomb("teste.txt", 4, 'w');
+    Golomb golomb("teste.txt", 2, 'w');
 
-    vector<bool> v = golomb.encode(-20);
+    vector<bool> v = golomb.encode(13);
+    vector<bool> v2 = golomb.encode(10);
 
+    cout << "encoded number: ";
     for (int i = 0; i < v.size(); i++) {
-        cout << v.at(i) << "\n";
+        cout << v.at(i);
     }
 
-    cout << golomb.decode(v);
+    cout << "\n" << "decoded number: " << golomb.decode(v) << "\n";
+
+    BitStream bsw("testGolomb.bin", 'w');
+
+    cout << "\n";
+
+    for (int i = 0; i < v.size(); i++) {
+        bsw.writeBit(v.at(i));
+        cout << v.at(i);
+    }
+
+    cout << "\n";
+
+    for (int i = 0; i < v2.size(); i++) {
+        bsw.writeBit(v2.at(i));
+        cout << v2.at(i);
+    }
+    cout << "\n";
+    cout << "\n";
+
+    /*
+    bsw.writeBit(1);
+    bsw.writeBit(0);
+    bsw.writeBit(1);
+    bsw.writeBit(1);
+    bsw.writeBit(1);
+    bsw.writeBit(1);
+    bsw.writeBit(0);
+    bsw.writeBit(0);
+    */
+
+    bsw.closeFO();
+
+    BitStream bsr("testGolomb.bin", 'r');
+
+    cout << "\n";
+    cout << "\n";
+
+    bsr.readFile();
+    bsr.closeFI();
+
 }
