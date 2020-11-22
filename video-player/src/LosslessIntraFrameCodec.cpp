85417273cb65f306e74f34c43d03a097484f48d2
Eduardo Santos <eduardosantoshf@gmail.com>
2020-11-22 16:38:52 +0000

    Changed some stuff

--- video-player/src/LosslessIntraFrameCodec.cpp
+++ video-player/src/LosslessIntraFrameCodec.cpp
@@ -29,7 +29,7 @@ class LosslessIntraFrameCodec {
                 if (frame.empty()) break;
             }
 
-            predictor = new Predictor(resultFile, videoFormat, predictorType, m, c);
+            predictor = new Predictor(resultFile, videoFormat, predictorType, m, c, 1);
         }
 
         void losslessEncode() {
@@ -39,9 +39,7 @@ class LosslessIntraFrameCodec {
 
             vector<Mat> channels;
 
-            string encodingType = "encodeJPEG" + to_string(predictorType);
-
-            if (predictorType >= 1 && predictorType <= 7) {
+            if (predictorType == 1) {
                 while(1) {
                     cap >> frame;
 
@@ -49,9 +47,87 @@ class LosslessIntraFrameCodec {
 
                     split(frame, channels);
 
-                    predictor->encodingType(channels[0]);
-                    predictor->encodingType(channels[1]);
-                    predictor->encodingType(channels[2]);
+                    predictor->encodeJPEG1(channels[0]);
+                    predictor->encodeJPEG1(channels[1]);
+                    predictor->encodeJPEG1(channels[2]);
+                }
+            }
+            else if (predictorType == 2) {
+                while(1) {
+                    cap >> frame;
+
+                    if (frame.empty()) break;
+
+                    split(frame, channels);
+
+                    predictor->encodeJPEG2(channels[0]);
+                    predictor->encodeJPEG2(channels[1]);
+                    predictor->encodeJPEG2(channels[2]);
+                }
+            }
+            else if (predictorType == 3) {
+                while(1) {
+                    cap >> frame;
+
+                    if (frame.empty()) break;
+
+                    split(frame, channels);
+
+                    predictor->encodeJPEG3(channels[0]);
+                    predictor->encodeJPEG3(channels[1]);
+                    predictor->encodeJPEG3(channels[2]);
+                }
+            }
+            else if (predictorType == 4) {
+                while(1) {
+                    cap >> frame;
+
+                    if (frame.empty()) break;
+
+                    split(frame, channels);
+
+                    predictor->encodeJPEG4(channels[0]);
+                    predictor->encodeJPEG4(channels[1]);
+                    predictor->encodeJPEG4(channels[2]);
+                }
+            }
+            else if (predictorType == 5) {
+                while(1) {
+                    cap >> frame;
+
+                    if (frame.empty()) break;
+
+                    split(frame, channels);
+
+                    predictor->encodeJPEG5(channels[0]);
+                    predictor->encodeJPEG5(channels[1]);
+                    predictor->encodeJPEG5(channels[2]);
+                }
+            }
+            else if (predictorType == 6) {
+                while(1) {
+                    cap >> frame;
+
+                    if (frame.empty()) break;
+
+                    split(frame, channels);
+
+                    predictor->encodeJPEG6(channels[0]);
+                    predictor->encodeJPEG6(channels[1]);
+                    predictor->encodeJPEG6(channels[2]);
+                }
+            }
+            else if (predictorType == 7) {
+                while(1) {
+                    cap >> frame;
+
+                    if (frame.empty()) break;
+
+                    split(frame, channels);
+
+                    predictor->encodeJPEG7(channels[0]);
+                    predictor->encodeJPEG7(channels[1]);
+                    predictor->encodeJPEG7(channels[2]);
                 }
             }
             else {
@@ -69,7 +145,7 @@ class LosslessIntraFrameCodec {
             }
         }
 
-        int losslessDecode() {
+        void losslessDecode() {
             Mat decodedFrame;
             Mat decodedChannel0;
             Mat decodedChannel1;
@@ -77,13 +153,95 @@ class LosslessIntraFrameCodec {
 
             vector<Mat> decodedChannels;
 
-            string decodingType = "decodeJPEG" + to_string(predictor->getPredictorType());
-
-            if (predictor->getPredictorType() >= 1 && predictor->getPredictorType() <= 7) {
+            if (predictor->getPredictorType() == 1) {
                 for (int i = 0; i < predictor->getFramesNumber(); i++) {
-                    decodedChannel0 = predictor->decodingType();
-                    decodedChannel1 = predictor->decodingType();
-                    decodedChannel2 = predictor->decodingType();
+                    decodedChannel0 = predictor->decodeJPEG1();
+                    decodedChannel1 = predictor->decodeJPEG1();
+                    decodedChannel2 = predictor->decodeJPEG1();
+
+                    decodedChannels.push_back(decodedChannel0);
+                    decodedChannels.push_back(decodedChannel1);
+                    decodedChannels.push_back(decodedChannel2);
+
+                    merge(decodedChannels, decodedFrame);
+                    imshow("Decoded Frame", decodedFrame);
+                }
+            }
+            else if (predictor->getPredictorType() == 2) {
+                for (int i = 0; i < predictor->getFramesNumber(); i++) {
+                    decodedChannel0 = predictor->decodeJPEG2();
+                    decodedChannel1 = predictor->decodeJPEG2();
+                    decodedChannel2 = predictor->decodeJPEG2();
+
+                    decodedChannels.push_back(decodedChannel0);
+                    decodedChannels.push_back(decodedChannel1);
+                    decodedChannels.push_back(decodedChannel2);
+
+                    merge(decodedChannels, decodedFrame);
+                    imshow("Decoded Frame", decodedFrame);
+                }
+            }
+            else if (predictor->getPredictorType() == 3) {
+                for (int i = 0; i < predictor->getFramesNumber(); i++) {
+                    decodedChannel0 = predictor->decodeJPEG3();
+                    decodedChannel1 = predictor->decodeJPEG3();
+                    decodedChannel2 = predictor->decodeJPEG3();
+
+                    decodedChannels.push_back(decodedChannel0);
+                    decodedChannels.push_back(decodedChannel1);
+                    decodedChannels.push_back(decodedChannel2);
+
+                    merge(decodedChannels, decodedFrame);
+                    imshow("Decoded Frame", decodedFrame);
+                }
+            }
+            else if (predictor->getPredictorType() == 4) {
+                for (int i = 0; i < predictor->getFramesNumber(); i++) {
+                    decodedChannel0 = predictor->decodeJPEG4();
+                    decodedChannel1 = predictor->decodeJPEG4();
+                    decodedChannel2 = predictor->decodeJPEG4();
+
+                    decodedChannels.push_back(decodedChannel0);
+                    decodedChannels.push_back(decodedChannel1);
+                    decodedChannels.push_back(decodedChannel2);
+
+                    merge(decodedChannels, decodedFrame);
+                    imshow("Decoded Frame", decodedFrame);
+                }
+            }
+            else if (predictor->getPredictorType() == 5) {
+                for (int i = 0; i < predictor->getFramesNumber(); i++) {
+                    decodedChannel0 = predictor->decodeJPEG5();
+                    decodedChannel1 = predictor->decodeJPEG5();
+                    decodedChannel2 = predictor->decodeJPEG5();
+
+                    decodedChannels.push_back(decodedChannel0);
+                    decodedChannels.push_back(decodedChannel1);
+                    decodedChannels.push_back(decodedChannel2);
+
+                    merge(decodedChannels, decodedFrame);
+                    imshow("Decoded Frame", decodedFrame);
+                }
+            }
+            else if (predictor->getPredictorType() == 6) {
+                for (int i = 0; i < predictor->getFramesNumber(); i++) {
+                    decodedChannel0 = predictor->decodeJPEG6();
+                    decodedChannel1 = predictor->decodeJPEG6();
+                    decodedChannel2 = predictor->decodeJPEG6();
+
+                    decodedChannels.push_back(decodedChannel0);
+                    decodedChannels.push_back(decodedChannel1);
+                    decodedChannels.push_back(decodedChannel2);
+
+                    merge(decodedChannels, decodedFrame);
+                    imshow("Decoded Frame", decodedFrame);
+                }
+            }
+            else if (predictor->getPredictorType() == 7) {
+                for (int i = 0; i < predictor->getFramesNumber(); i++) {
+                    decodedChannel0 = predictor->decodeJPEG7();
+                    decodedChannel1 = predictor->decodeJPEG7();
+                    decodedChannel2 = predictor->decodeJPEG7();
 
                     decodedChannels.push_back(decodedChannel0);
                     decodedChannels.push_back(decodedChannel1);
