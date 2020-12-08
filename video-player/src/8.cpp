dfecb4a5696c1d81c682819ab5a205e412af06e3
Eduardo Santos <eduardosantoshf@gmail.com>
2020-12-08 22:56:34 +0000

    Minor fix

--- video-player/src/8.cpp
+++ video-player/src/8.cpp
@@ -6,10 +6,11 @@ using namespace std;
 
 int main (int argc, char** argv) {
 
-    LosslessIntraFrameCodec l(argv[1],"LosslessEncoded_p7_m4.bin", 0, 6, 4, 1);
+    LosslessIntraFrameCodec l(argv[1],"teste_p8_m4.bin", 0, 8, 4, 1);
     l.losslessEncode();
 
     //LosslessIntraFrameCodec l2("", argv[1], 0, 0, 0, 0);
     //l2.losslessDecode();
+
 	return 0;
 }
