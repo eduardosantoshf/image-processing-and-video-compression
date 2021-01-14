bcff18908d3af273630ee35170f9414d312b30a0
Eduardo Santos <eduardosantoshf@gmail.com>
2021-01-14 02:36:38 +0000

    Minor changes

--- video-player/src/Predictor.cpp
+++ video-player/src/Predictor.cpp
@@ -68,6 +68,7 @@ class Predictor {
 		   	}else{
 		   		RBitStream rbs(file);
 		   		od = 0;
+
 		   		typeVideo = rbs.readNBits(8);
 		   		type = rbs.readNBits(8);
 		   		m = rbs.readNBits(8);
@@ -77,15 +78,17 @@ class Predictor {
 		   		colunas = stoi(rbs.readString(4));
 		   		linhas = stoi(rbs.readString(4));
 		   		n_frames = stoi(rbs.readString(4));
-		   		cout << "Type video: " << typeVideo << endl;
-		   		cout << "Type : " << type<< endl;
-		   		cout << "m: " << m << endl;
-		   		cout << "colunas: " << colunas << endl;
-		   		cout << "linhas: " << linhas<< endl;
-		   		cout << "n_frames: " << n_frames<< endl;
+
+		   		cout << "Type of video: " << typeVideo << endl;
+		   		cout << "Type of predictor: " << type << endl;
+		   		cout << "M: " << m << endl;
+		   		cout << "Columns: " << colunas << endl;
+		   		cout << "Lines: " << linhas<< endl;
+		   		cout << "Number of frames: " << n_frames<< endl;
 		   		cout << "Search space: " << search_space << endl;
 		   		cout << "Block size: " << block_size << endl;
 		   		cout << "Period: " << period << endl;
+
 		   		rbs.close();
 		   		g = new Golomb(file,m,0);
 				g->SkipNBytes(18);
@@ -156,54 +159,55 @@ class Predictor {
 				 * @param planes channels of the frame
 				 */
 
-				int lin;
-				int col;
+				 Mat current_block, possible_block, min_block;
+
+				int lin, col;
 				int count = 0;
-				int count_blocks= 0;
-				Mat current_block;
-				Mat possible_block;
-				Mat min_block;
+				int count_blocks = 0;
 				int lastSum = 100000;
 				int currentSum = 0;
-				int d_x=0;
-				int d_y=0;
-	    		for(Mat matrix :planes){
+				int d_x = 0;
+				int d_y = 0;
+	    		for (Mat matrix :planes) {
 	    			lin = matrix.rows;
 	    			col = matrix.cols;
-	    			for(int c = 0; c < lin; c+=block_size){
-					for(int i = 0; i < col; i+=block_size){
-						current_block = matrix.colRange(i,i+block_size).rowRange(c,c+block_size);
-						for(int k = c-search_space; k < c+search_space+block_size; k++){
-							for(int j = i-search_space; j < i +search_space+block_size  ; j++){
-								if (k+block_size < lin && k >= 0 && j >= 0 && j+block_size < col){
-									possible_block = lastFrame[count].colRange(j,j+block_size).rowRange(k,k+block_size);
-									for(int a = 0; a < block_size; a++){
-										for(int b = 0; b < block_size; b++){
-											currentSum = currentSum + abs(((int)current_block.at<uchar>(a,b)-(int)possible_block.at<uchar>(a,b)));
+
+	    			for(int c = 0; c < lin; c += block_size){
+						for(int i = 0; i < col; i += block_size){
+							current_block = matrix.colRange(i, i + block_size).rowRange(c, c + block_size);
+							for(int k = c - search_space; k < c + search_space + block_size; k++){
+								for(int j = i - search_space; j < i + search_space + block_size; j++){
+									if (k + block_size < lin && k >= 0 && j >= 0 && j + block_size < col){
+										possible_block = lastFrame[count].colRange(j, j + block_size).rowRange(k, k + block_size);
+										for(int a = 0; a < block_size; a++){
+											for(int b = 0; b < block_size; b++){
+												currentSum = currentSum + abs(((int)current_block.at<uchar>(a, b)-(int)possible_block.at<uchar>(a, b)));
+											}
+										}
+										if (currentSum < lastSum){
+											lastSum = currentSum;
+											d_x = k;
+											d_y = j;
+											min_block = possible_block;
 										}
 									}
-									if (currentSum < lastSum){
-										lastSum = currentSum;
-										d_x = k;
-										d_y = j;
-										min_block = possible_block;
-									}
+									currentSum = 0;
 								}
-								currentSum = 0;
 							}
-						}
-						lastSum = 10000;
-						g->encode(c-d_x);
-						g->encode(i-d_y);
-						for(int n = 0; n < block_size; n++){
-							for(int m = 0; m < block_size; m++){
-								int z= (int)((int)min_block.at<uchar>(n,m)-(int)current_block.at<uchar>(n,m));
-								g->encode(z);
+							lastSum = 10000;
+
+							g->encode(c - d_x);
+							g->encode(i - d_y);
+
+							for(int n = 0; n < block_size; n++){
+								for(int m = 0; m < block_size; m++){
+									int z= (int)((int)min_block.at<uchar>(n, m) - (int)current_block.at<uchar>(n, m));
+									g->encode(z);
+								}
 							}
+							count_blocks++;
 						}
-						count_blocks++;
 					}
-				}
 				count ++;
 	    		}
 	    	}
@@ -224,17 +228,17 @@ class Predictor {
 	    			int y;
 	    			int count = 0;
 	    			for(int c = 0; c < linhas; c+=block_size){
-					for(int i = 0; i < colunas; i+=block_size){
-						x = -(g->decode()-c);
-						y = -(g->decode()-i);
-						current_block = lastFrame[count].colRange(y,y+block_size).rowRange(x,x+block_size);
-						for(int a = 0; a < block_size; a++){
-							for(int b = 0; b < block_size; b++){
-							m1.at<uchar>(c+a,i+b) = (unsigned char)-(g->decode()-(int) current_block.at<uchar>(a,b));							
-							}
-						}				
+						for(int i = 0; i < colunas; i+=block_size){
+							x = -(g->decode()-c);
+							y = -(g->decode()-i);
+							current_block = lastFrame[count].colRange(y,y+block_size).rowRange(x,x+block_size);
+							for(int a = 0; a < block_size; a++){
+								for(int b = 0; b < block_size; b++){
+								m1.at<uchar>(c+a,i+b) = (unsigned char)-(g->decode()-(int) current_block.at<uchar>(a,b));							
+								}
+							}				
+						}
 					}
-				}
 				count++;
 				for(int c = 0; c < linhas; c+=block_size){
 					for(int i = 0; i < colunas; i+=block_size){
@@ -276,17 +280,17 @@ class Predictor {
 	    			int decoded;
 	    			int pixel;
 	    			for(int c = 0; c < linhas; c+=block_size){
-					for(int i = 0; i < colunas; i+=block_size){
-						x = -(g->decode()-c);
-						y = -(g->decode()-i);
-						current_block = lastFrame[count].colRange(y,y+block_size).rowRange(x,x+block_size);
-						for(int a = 0; a < block_size; a++){
-							for(int b = 0; b < block_size; b++){
-								m1.at<uchar>(c+a,i+b) = (unsigned char)-(g->decode()-(int) current_block.at<uchar>(a,b));							
-							}
-						}				
+						for(int i = 0; i < colunas; i+=block_size){
+							x = -(g->decode()-c);
+							y = -(g->decode()-i);
+							current_block = lastFrame[count].colRange(y,y+block_size).rowRange(x,x+block_size);
+							for(int a = 0; a < block_size; a++){
+								for(int b = 0; b < block_size; b++){
+									m1.at<uchar>(c+a,i+b) = (unsigned char)-(g->decode()-(int) current_block.at<uchar>(a,b));							
+								}
+							}				
+						}
 					}
-				}
 				count++;
 				for(int c = 0; c < linhas; c+=block_size){
 					for(int i = 0; i < colunas/2; i+=block_size){
@@ -342,17 +346,17 @@ class Predictor {
 	    			int decoded;
 	    			int pixel;
 	    			for(int c = 0; c < linhas; c+=block_size){
-					for(int i = 0; i < colunas; i+=block_size){
-						x = -(g->decode()-c);
-						y = -(g->decode()-i);
-						current_block = lastFrame[count].colRange(y,y+block_size).rowRange(x,x+block_size);
-						for(int a = 0; a < block_size; a++){
-							for(int b = 0; b < block_size; b++){
-								m1.at<uchar>(c+a,i+b) = (unsigned char)-(g->decode()-(int) current_block.at<uchar>(a,b));							
-							}
-						}				
+						for(int i = 0; i < colunas; i+=block_size){
+							x = -(g->decode()-c);
+							y = -(g->decode()-i);
+							current_block = lastFrame[count].colRange(y,y+block_size).rowRange(x,x+block_size);
+							for(int a = 0; a < block_size; a++){
+								for(int b = 0; b < block_size; b++){
+									m1.at<uchar>(c+a,i+b) = (unsigned char)-(g->decode()-(int) current_block.at<uchar>(a,b));							
+								}
+							}				
+						}
 					}
-				}
 				count++;
 				for(int c = 0; c < linhas/2; c+=block_size){
 					for(int i = 0; i < colunas/2; i+=block_size){
